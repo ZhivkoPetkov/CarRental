fddfc1f68655e8518091fed6b3820b940300fc14
zhivkopetkov <azzurri@abv.bg>
2019-06-28 00:18:58 +0300

    implemented a search form on index page, view only

--- Services/CarRental.Services/CarRental.Services/Contracts/ICarsService.cs
+++ Services/CarRental.Services/CarRental.Services/Contracts/ICarsService.cs
@@ -1,12 +1,11 @@
 ﻿using CarRental.Models;
-using System;
-using System.Collections.Generic;
-using System.Text;
 
 namespace CarRental.Services.Contracts
 {
     public interface ICarsService
     {
         bool AddCar(Car car);
+
+
     }
 }
--- Web/CarRental.Web/Controllers/HomeController.cs
+++ Web/CarRental.Web/Controllers/HomeController.cs
@@ -1,18 +1,25 @@
 ﻿namespace CarRental.Web.Controllers
 {
+    using CarRental.Services.Contracts;
+    using CarRental.Web.ViewModels.Home;
     using Microsoft.AspNetCore.Mvc;
 
     public class HomeController : BaseController
     {
-        public IActionResult Index()
+        private readonly ILocationsService locationsService;
+
+        public HomeController(ILocationsService locationsService)
         {
-            return this.View();
+            this.locationsService = locationsService;
         }
 
-        public IActionResult Privacy()
+        public IActionResult Index()
         {
-            return this.View();
+            var locationsList = this.locationsService.GetAllLocationNames();
+
+            return this.View(new SearchCarsViewModel { Locations = locationsList });
         }
+
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error() => this.View();
