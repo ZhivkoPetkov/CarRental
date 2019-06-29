9f833135e9e833294da9a3ea4328e08505656a6b
zhivkopetkov <azzurri@abv.bg>
2019-06-29 20:28:36 +0300

    added viewcomponent for company details

---
+++ Web/CarRental.Web/ViewComponents/Home/CompanyDetailsViewComponent.cs
@@ -0,0 +1,37 @@
+﻿using CarRental.Data;
+using CarRental.Web.ViewModels.Home;
+using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CarRental.Web.ViewComponents.Home
+{
+    public class CompanyDetailsViewComponent : Microsoft.AspNetCore.Mvc.ViewComponent
+    {
+        private readonly CarRentalDbContext dbContext;
+
+        public CompanyDetailsViewComponent(CarRentalDbContext dbContext)
+        {
+            this.dbContext = dbContext;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var items = await GetStats();
+            return View(items);
+        }
+
+        private Task<CompanyDetailsViewModel> GetStats()
+        {
+            var model = new CompanyDetailsViewModel
+            {
+                Cars = this.dbContext.Cars.Count(),
+                Clients = this.dbContext.Users.Count(),
+                Rating = this.dbContext.Reviews.Count() == 0 ? 0 : this.dbContext.Reviews.Average(p => p.Rating),
+                Reviews = this.dbContext.Reviews.Count()
+            };
+
+            return Task.FromResult(model);
+        }
+    }
+}
---
+++ Web/CarRental.Web/ViewModels/Home/CompanyDetailsViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CarRental.Web.ViewModels.Home
+{
+    public class CompanyDetailsViewModel
+    {
+        public int Clients { get; set; }
+        public int Cars { get; set; }
+        public int Reviews { get; set; }
+        public double Rating { get; set; }
+    }
+}
