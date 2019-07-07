910126eea8e8a07840e4cb2d078a222537ea89a0
zhivkopetkov <azzurri@abv.bg>
2019-07-07 14:36:53 +0300

    added basic admin panel

---
+++ Web/CarRental.Web/Areas/Administration/Controllers/HomeController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CarRental.Web.Areas.Administration.Controllers
+{
+    public class HomeController : AdministrationController
+    {
+        public IActionResult AdminPanel()
+        {
+            return View();
+        }
+    }
+}
